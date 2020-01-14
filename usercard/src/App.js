90c49991e1d8ca3937a8cdeb9b0b99b7ccb5b1b4
Gerard Crockenberg <gmahred@gmail.com>
2020-01-14 15:48:39 -0500

    tweaked search functionality

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -36,6 +36,24 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.search !== prevState.search) {
+      console.log('Inside first if');
+      if (this.state.search === "") {
+        console.log('Inside second if');
+        axios
+          .get('https://api.github.com/users/juhrard/followers')
+          .then(response => {
+            this.setState({
+              users: response.data
+            });
+            console.log(response);
+          })
+          .catch(error => console.log(error));
+      }
+    }
+  }
+
   handleChanges = e => {
     this.setState({
       search: e.target.value
@@ -45,7 +63,7 @@ class App extends Component {
   fetchUser = e => {
     e.preventDefault();
     axios
-      .get(`https://api.github.com/users/${this.state.search}/`)
+      .get(`https://api.github.com/users/${this.state.search}`)
       .then(response => {
         this.setState({
           users: [response.data]
